532fba28530b556b1c055a74ff7d4c268559c8cc
HeadShotBoom <DCarroll@fullsail.edu>
2014-07-08 15:59:45 -0500

    Intro complete

--- Intro to Python/main.py
+++ Intro to Python/main.py
@@ -14,12 +14,72 @@ current_year = 2014
 age = current_year - birth_year
 # print "You are "+ str(age) +" years old."
 
+'''
 budget = raw_input("How much money you got?   ")
 
 if int(budget) >= 100:
     brand = "nike"
-    print "You can buy cool "+brand+" shoes"
+    # print "You can buy cool "+brand+" shoes"
 elif int(budget) >= 50:
-    print "You should save for better shoes"
+    # print "You should save for better shoes"
+    pass
 else:
-    print "No shoes for me."
+    # print "No shoes for me."
+    pass
+'''
+
+
+characters = ["Harry", "Hagrid", "Hermonie", "Ron"]
+characters.append("Daniel")
+# print characters[2]
+
+movies = dict() #Create Dictionary object
+movies = {"Harry Potter": "Voldemort", "The Departed":"Leo Decap"}
+# print movies["Harry Potter"]
+
+'''
+#while loop------
+i = 0
+while i<9:
+    print "The count is ", i
+    i = i+1
+
+
+#for loop------
+for i in range (0,10):
+    print "The count is ", i
+    i = i+1
+'''
+
+#"FOR EACH" Loop-----
+rappers = ["Tupac", "Biggie", "JaRule"]
+for r in rappers:
+    # print "One of the best rappers is "+r
+    pass
+
+#FUNCTIONS-------------
+x = 2
+
+def calcArea(h, w):
+    area = h * w
+    return area + x
+
+a = calcArea(20, 100);
+# print "My area is "+str(a)+" sq feet."
+print a
+
+title = "Contact Us"
+body = "You can contact us at Contact@us.com"
+message = '''
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>{title}</title>
+    </head>
+    <body>
+        {body}
+    </body>
+</html>
+'''
+message = message.format(**locals())
+print message
