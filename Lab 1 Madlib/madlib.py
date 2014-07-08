f7f006a54eb50e37dd1600aee645dbcaa390a47c
HeadShotBoom <DCarroll@fullsail.edu>
2014-07-08 17:37:31 -0500

    mad lib created

---
+++ Lab 1 Madlib/madlib.py
@@ -0,0 +1,18 @@
+#Professors Name
+#Class Name
+#AGE
+#Plural Noun
+#SECONDS I CAN HOLD BREATH
+#mode of transportation
+#Body Part
+#ACTIVITIES FOR GRANDPARENTS
+
+
+'''
+Dear PROFESSORS NAME
+I am currently in your CLASS NAME class for the AGE time and I am wondering if I could get an extension on my homework about PLURAL NOUN.
+I won’t be able to attend class for the next SECONDS I CAN HOLD BREATH week due to a MODE OF TRANSPORTATION accident. I also broke my BODY PART
+while ACTIVITIES FOR GRANDPARENTS so I am unable to operate the complexities of FSO 3.0.
+I feel EMOTION that you are giving me this exception, and if necessary ill give you $HOW MANY FINGERS to give me a 100 on this assignment.
+Sincerely,
+NICKNAME'''
