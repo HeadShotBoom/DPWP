9036b3fa73fc56fe96001c0134a499b75510ac70
HeadShotBoom <DCarroll@fullsail.edu>
2014-07-08 18:35:38 -0500

    Convert inputs to array

--- Lab 1 Madlib/madlib.py
+++ Lab 1 Madlib/madlib.py
@@ -11,17 +11,21 @@
 
 # Dear PROFESSORS NAME
 # I am currently in your CLASS NAME class for the AGE time and I am wondering if I could get an extension on my homework about PLURAL NOUN.
-# I won’t be able to attend class for the next SECONDS I CAN HOLD BREATH week due to a MODE OF TRANSPORTATION accident. I also broke my BODY PART
-# while ACTIVITIES FOR GRANDPARENTS so I am unable to operate the complexities of FSO 3.0.
+# I wont be able to attend class for the next SECONDS I CAN HOLD BREATH week due to a MODE OF TRANSPORTATION accident.
+# I also broke my BODY PART while ACTIVITIES FOR GRANDPARENTS so I am unable to operate the complexities of FSO 3.0.
 # I feel EMOTION that you are giving me this exception, and if necessary ill give you $HOW MANY FINGERS to give me a 100 on this assignment.
 # Sincerely,
 # NICKNAME
 
-instructor_name = raw_input("What is your instructors name?  ")
-class_name = raw_input("What is the name of the class?  ")
-your_age = raw_input("What is your current age?  ")
-plural_noun = raw_input("Enter a plural noun.  ")
-breath_seconds = raw_input("How many seconds can you hold your breath?  ")
-transport_mode = raw_input("How do you normally travel long distances?  ")
-body_part = raw_input("Favorite body part?  ")
-old_activities = raw_input("What do your grandparents do for fun?  ")
+answers = []
+
+answers.append(raw_input("What is your instructors name?  "))
+answers.append(raw_input("What is the name of the class?  "))
+answers.append(raw_input("What is your current age?  "))
+answers.append(raw_input("Enter a plural noun.  "))
+answers.append(raw_input("How many seconds can you hold your breath?  "))
+answers.append(raw_input("How do you normally travel long distances?  "))
+answers.append(raw_input("Favorite body part?  "))
+answers.append(raw_input("What do your grandparents do for fun?  "))
+
+print answers
