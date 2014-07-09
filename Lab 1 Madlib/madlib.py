1d05e4a23d753dd745dc6b580de4373dd6368cf2
HeadShotBoom <DCarroll@fullsail.edu>
2014-07-09 13:38:02 -0500

    Loop FINALLY Complete

--- Lab 1 Madlib/madlib.py
+++ Lab 1 Madlib/madlib.py
@@ -35,13 +35,34 @@ answers.append(raw_input("How many fingers do you think I am holding up right no
 answers.append(raw_input("Nickname your girlfriend/boyfriend calls you.  "))
 
 story = dict() #Create Dictionary Object
-story = {0: "Dear ", 1: " I am currently in your ", 2: " class for the ", 3: "  time and I am wondering if I could get an extension on my homework about ", 4: " I wont be able to attend class for the next ", 5: " weeks due to a ", 6: " accident. ", 7: " I also broke my ", 8: " while ", 9: " so I am unable to operate the complexities of FSO 3.0. ", 10: " I feel ", 11: " that you are giving me this exception, and if necessary ill give you $", 12: " to give me a 100 on this assignment.", 13: "Sincerely,"}
+story = {0: "Dear ", 1: " I am currently in your ", 2: " class for the ", 3: "th time and I am wondering if I could get an extension on my homework about ", 4: " I wont be able to attend class for the next ", 5: " weeks due to a ", 6: " accident. ", 7: " I also broke my ", 8: " while ", 9: " so I am unable to operate the complexities of FSO 3.0. ", 10: " I feel ", 11: " that you are giving me this exception, and if necessary ill give you $", 12: " to give me a 100 on this assignment.", 13: "Sincerely,"}
+
 
 line_number = 0
-for s in story:
-    if int(line_number) < 1:
-        final_story = story[s]+answers[s]
+while line_number < 12:
+    if int(line_number) == 0:
+        final_story = story[line_number]+answers[line_number]
+        print final_story
+        line_number += 1
+    elif int(line_number) > 0 and int(line_number) < 3:
+        final_story = story[line_number]+answers[line_number]+story[line_number+1]+answers[line_number+1]+story[line_number+2]+answers[line_number+2]+"."
+        print final_story
+        line_number += 3
+    elif int(line_number) == 4:
+        final_story = story[line_number]+answers[line_number]+story[line_number+1]+answers[line_number+1]+story[line_number+2]
+        print final_story
+        line_number += 3
+    elif int(line_number) == 7:
+        final_story = story[line_number]+answers[line_number-1]+story[line_number+1]+answers[line_number]+story[line_number+2]
+        print final_story
+        line_number += 3
+    elif int(line_number) == 10:
+        final_story = story[line_number]+answers[line_number-2]+story[line_number+1]+answers[line_number-1]+story[line_number+2]
+        print final_story
+        line_number += 1
+    elif int(line_number) == 11:
+        print story[13]
+        print answers[10]
         line_number += 1
     else:
         pass
-print final_story
