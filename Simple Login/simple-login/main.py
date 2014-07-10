7c5e2da4cc8464b877eecb3b736872e4d4fcf150
HeadShotBoom <DCarroll@fullsail.edu>
2014-07-09 19:46:25 -0500

    Login complete

--- Simple Login/simple-login/main.py
+++ Simple Login/simple-login/main.py
@@ -1,22 +1,31 @@
-'''
-Daniel Carroll
-9 July 2014
-Simple Login
-'''
-import webapp2  # use the webapp2 library
 
+import webapp2  # use the webapp2 library
 
 class MainHandler(webapp2.RequestHandler):  # declaring a class
     def get(self):  # function that starts everything. Catalyst
-        self.response.write('Hello world!')
-        #code goes here
+        about_button = Button()
+        about_button.label = "About Us"
+        about_button.show_label()
+        contact_button = Button()
+        contact_button.label = "Contact Us"
+        contact_button.show_label()
 
-    def additional_functions(self):
-        pass
-        #code goes here
+class Button(object):
+    def __init__(self):
+        self.label = ""  # public attribute
+        self.__size = 60  # Private attribute - two underscores
+        self._color = "0x00000"  # Protected attribute - one underscore
+        # self.on_roll_over("Hello!!")
 
+    def click(self):
+        print "I've been clicked"
 
-# never touch this
+    def on_roll_over(self, message):
+        print "You've rolled over this " + message
+
+    def show_label(self):
+        print "My Label is " + self.label
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
