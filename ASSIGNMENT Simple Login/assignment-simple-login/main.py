ef31122e2e6f1574349123a4209cfa055a64852d
HeadShotBoom <DCarroll@fullsail.edu>
2014-07-14 14:57:20 -0500

    More Code

--- ASSIGNMENT Simple Login/assignment-simple-login/main.py
+++ ASSIGNMENT Simple Login/assignment-simple-login/main.py
@@ -8,7 +8,21 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        page = '''<!DOCTYPE HTML>
+<html>
+    <head>
+    <title>Simple Form</title>
+
+    </head>
+    <body>
+        <form method="GET">
+            <label>Name: <input type="text" name="user" /></label>
+            <label>Email: <input type="test" name="email" /></label>
+            <input type="submit" value="Submit" />
+        </form>
+    </body>
+</html>'''
+        self.response.write(page) #Prints information to page
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
