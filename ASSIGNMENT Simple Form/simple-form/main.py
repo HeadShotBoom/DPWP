8bce1c393b8a8680c54132ade54a2868ca2c7e3b
HeadShotBoom <DCarroll@fullsail.edu>
2014-07-14 23:42:42 -0500

    Python is horrible

--- ASSIGNMENT Simple Form/simple-form/main.py
+++ ASSIGNMENT Simple Form/simple-form/main.py
@@ -7,8 +7,18 @@ Simple Form
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
+
+
     def get(self):
-        page_head = '''<!DOCTYPE HTML>
+        p = Page()
+        self.response.write(p.print_out())
+
+
+class Page(object):
+    def __init__(self):
+        self.css = "css/styles.css"
+
+        self.page_head = '''<!DOCTYPE HTML>
 <html>
     <head>
     <title>Simple Form</title>
@@ -16,7 +26,7 @@ class MainHandler(webapp2.RequestHandler):
 
     </head>
     <body>'''
-        first_page = '''
+        self.first_page = '''
         <h2>Please enter your profile information</h2>
         <form method="GET">
             <label>Name: <input type="text" name="first" /></label>
@@ -32,24 +42,30 @@ class MainHandler(webapp2.RequestHandler):
             </select>
             <input type="submit" value="Submit" />
         </form>'''
-        page_close = '''
+        self.page_close = '''
     </body>
 </html>'''
         if self.request.GET:
-            first_name = self.request.GET["first"]
-            last_name = self.request.GET["last"]
-            email = self.request.GET["email"]
-            gender = self.request.GET["gender"]
-            mood = self.request.GET["mood"]
-            second_page = '''<h3>Please Verify Your Information Below</h3>
+            # first_name = self.request.GET["first"]
+            # last_name = self.request.GET["last"]
+            # email = self.request.GET["email"]
+            # gender = self.request.GET["gender"]
+            # mood = self.request.GET["mood"]
+            self.second_page = '''<h3>Please Verify Your Information Below</h3>
         <p>Name: ''' + first_name + " " + last_name + '''</p>
         <p>Email: ''' + email + '''</p>
         <p>Gender: ''' + gender + '''</p>
         <p>Mood: ''' + mood + '''</p>
         '''
-            self.response.write(page_head + second_page + page_close)
+            self.response.write(self.page_head + self.second_page + self.page_close)
         else:
-            self.response.write(page_head + first_page + page_close)
+            self.response.write(self.page_head + self.first_page + self.page_close)
+
+        # def print_out(self):
+        #     all = self.head + self.body + self.close
+        #     all = all.format(**locals())
+        #     return all
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
