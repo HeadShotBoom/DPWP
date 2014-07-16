4b7442a0a1d39fc50b2b10b625c1568d56924084
Daniel Carroll <DCarroll@fullsail.edu>
2014-07-15 21:52:46 -0500

    Basic Functionality added for Displaying HTML/CSS

--- ASSIGNMENT Encapsulated Calculator/encapsulated-calculator/main.py
+++ ASSIGNMENT Encapsulated Calculator/encapsulated-calculator/main.py
@@ -1,9 +1,17 @@
 
 import webapp2
+from pages import Page
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        p = Page()
+        p.title = "Encapsulated Calculator"
+        p.css = "css/styles.css"
+        p.body = "<h1>It Works</h1>"
+        self.response.write(p.whole_page)
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
