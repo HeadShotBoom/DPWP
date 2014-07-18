342e1bb953848d250943439e12777a5c5b231195
Daniel Carroll <DCarroll@fullsail.edu>
2014-07-17 21:52:48 -0500

    Initial Commit

---
+++ Fox Says What/fox-say/main.py
@@ -0,0 +1,15 @@
+
+import webapp2
+from animals import *
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+
+       
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
