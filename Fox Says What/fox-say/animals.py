f7d13ebfcf63f61c2883982223e694741e50063d
Daniel Carroll <DCarroll@fullsail.edu>
2014-07-17 21:58:02 -0500

    Abstract class made

---
+++ Fox Says What/fox-say/animals.py
@@ -0,0 +1,14 @@
+class Animal(object):
+    def __init__(self):
+
+        self.name = ""
+        self.phylum = ""
+        self.klass = ""
+        self.family = ""
+        self.genus = ""
+        self.url = ""
+        self.lifespan = ""
+        self.habitat = ""
+        self.geolocation = ""
+        self.sound = "audio/jungle.mp3"
+
