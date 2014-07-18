0761cb2019bba0ce29fcaadf5c581e5c6a423dda
Daniel Carroll <DCarroll@fullsail.edu>
2014-07-17 21:58:18 -0500

    First Specific class made

--- Fox Says What/fox-say/animals.py
+++ Fox Says What/fox-say/animals.py
@@ -12,3 +12,18 @@ class Animal(object):
         self.geolocation = ""
         self.sound = "audio/jungle.mp3"
 
+class Snake(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+
+        self.name = "Kobra"
+        self.phylum = "Chordata"
+        self.klass = "Reptilia"
+        self.family = ""
+        self.genus = "Varanus"
+        self.url = "images/snake.png"
+        self.lifespan = "7 Years"
+        self.habitat = "Zoo"
+        self.geolocation = "Komodo Island"
+        self.sound = "audio/snake.mp3"
+
