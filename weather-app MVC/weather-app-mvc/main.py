8c29cba865372ed5703115d4914910772cd0a66a
Daniel Carroll <DCarroll@fullsail.edu>
2014-07-31 19:08:02 -0500

    Slightly Back On Track

--- weather-app MVC/weather-app-mvc/main.py
+++ weather-app MVC/weather-app-mvc/main.py
@@ -12,12 +12,13 @@ class MainHandler(webapp2.RequestHandler):
 
         if self.request.GET:
             #get info from the API
-            mm = MovieModel() #Creates Model
-            self.movie = self.request.GET['movie'] # Sends zip from URL to model
-            mm.movie = self.movie.replace(" ","+")
+            mm = MovieModel()  #Creates Model
+            this_movie = self.request.GET['movie'] # Sends Movie from URL to model
+            new_movie = this_movie.replace(" ", "+")
+            mm.movie = new_movie
             mm.callApi()
             mv = MovieView()
-            mv.wdos = mm._movie  # Takes data objects from model and gives to view.
+            mv.wdos = mm.current_movie  # Takes data objects from model and gives to view.
             p._body = mv.content
             self.response.write(p.print_out())
         else:
@@ -29,30 +30,78 @@ class MovieView(object):
     """
     def __init__(self):
         self.__wdos = []
-        self.__content = '<h3>The movie you selected is '
+        self.__content = '<h3>The movie you selected is.<h3>'
+ #         + ' .</h3>' + '<p>It has a runtime of ' + \
+ # md.length + ' minutes.</p>' + '<p>It has a rating of ' + do.critic_rating + ' .</p>' + \
+ # '<p>The main actor is ' + do.actor + ' .</p>' + '<h4>Box Image</h4>' +'<img src="' + \
+ # do.thumbnail + '" width="20" />'
 
-                         # + do.title + ' .</h3>' + '<p>It has a runtime of ' + \
-                         # md.length + ' minutes.</p>' + '<p>It has a rating of ' + do.critic_rating + ' .</p>' + \
-                         # '<p>The main actor is ' + do.actor + ' .</p>' + '<h4>Box Image</h4>' +'<img src="' + \
-                         # do.thumbnail + '" width="20" />'
 
 
     @property
     def wdos(self):
-        pass
+        return self.__wdos
 
     @wdos.setter
     def wdos(self, arr):
         self.__wdos = arr
 
     @property
+    def title(self):
+        return self.__title
+
+    @title.setter
+    def title(self, arr):
+        self.__title = arr
+
+    @property
+    def length(self):
+        return self.__length
+
+    @length.setter
+    def length(self, arr):
+        self.__length = arr
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @rating.setter
+    def rating(self, arr):
+        self.__rating = arr
+
+    @property
+    def thumbnail(self):
+        return self.__thumbnail
+
+    @thumbnail.setter
+    def thumbnail(self, arr):
+        self.__thumbnail = arr
+
+    @property
+    def thumbnail(self):
+        return self.__thumbnail
+
+    @thumbnail.setter
+    def thumbnail(self, arr):
+        self.__thumbnail = arr
+
+
+    @property
+    def actor(self):
+        return self.__actor
+
+    @actor.setter
+    def actor(self, new):
+        self.__actor = new
+
+    @property
     def content(self):
-        self.__content = self.__content.format(**locals())
         return self.__content
 
     @content.setter
     def content(self, new):
-        pass
+        self.__content = new
 
 
 
@@ -61,12 +110,11 @@ class MovieModel(object):
     def __init__(self):
         self.jsondoc = ""
         self.__movie = ""
-        self.__current_movie = ""
-
 
     def callApi(self):
         #get info from the API
-        url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=zt3u3utgzsba8qdy3bwcaf6g&q=" + self.__movie + "&page_limit=1"
+        url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=zt3u3utgzsba8qdy3bwcaf6g&q=" + \
+              self.__movie + "&page_limit=1"
         #Assemble the request
         request = urllib2.Request(url)
         #Use URLLIB2 Library to create object to get url
@@ -77,12 +125,13 @@ class MovieModel(object):
         #parsoing the json
         jsondoc = json.load(result)
 
-        self.__current_movie.title = jsondoc['movies'][0]['title']
-        self.__current_movie.length = jsondoc['movies'][0]['runtime']
-        self.__current_movie.critic_rating = jsondoc['movies'][0]['ratings']['audience_score']
-        self.__current_movie.thumbnail = jsondoc['movies'][0]['posters']['thumbnail']
-        self.__current_movie.actor = jsondoc['movies'][0]['abridged_cast'][0]['name']
+        self.current_movie_title = jsondoc['movies'][0]['title']
+        self.current_movie_length = jsondoc['movies'][0]['runtime']
+        self.current_movie_critic_rating = jsondoc['movies'][0]['ratings']['audience_score']
+        self.current_movie_thumbnail = jsondoc['movies'][0]['posters']['thumbnail']
+        self.current_movie_actor = jsondoc['movies'][0]['abridged_cast'][0]['name']
 
+        self.__current_movie = [self.current_movie_title, self.current_movie_length, self.current_movie_critic_rating, self.current_movie_thumbnail, self.current_movie_actor]
     @property
     def current_movie(self):
         return self.__current_movie
@@ -90,15 +139,16 @@ class MovieModel(object):
     @current_movie.setter
     def current_movie(self, new):
         self.__current_movie = new
-
+        return self.__current_movie
 
     @property
     def movie(self):
-        pass
+        return self.__movie
 
     @movie.setter
     def movie(self, new_movie):
         self.__movie = new_movie
+        return self.__movie
 
 class MovieData(object):
     """
